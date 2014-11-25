521693d2d195a2eba964e1f2766d8a0cffdbd6b5
Julian Simioni <julian.simioni@gmail.com>
2014-11-25 21:47:35 +0100

    Add Scala Phone Number solution

---
+++ scala/phone-number/src/main/scala/phone_number.scala
@@ -0,0 +1,29 @@
+class PhoneNumber(input: String) {
+  def number() : String = {
+    val full_number = input.toCharArray.filter{ c =>
+      "1234567890".contains(c)
+    }.mkString
+
+    full_number.length match {
+      case 9 => "0000000000"
+      case 10 => full_number
+      case 11 => parse_11_digits(full_number)
+    }
+  }
+
+  def areaCode() : String = {
+    number().take(3)
+  }
+
+  override def toString() : String = {
+    "(" + areaCode() + ") " + number.substring(3, 6) + "-" + number.takeRight(4)
+  }
+
+  def parse_11_digits(full_number : String) : String = {
+    if (full_number(0) == '1') {
+      full_number.takeRight(10)
+    } else {
+      "0000000000"
+    }
+  }
+}
