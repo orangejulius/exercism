ac57fd157af537461527cda8f5613b232b38ea4d
Julian Simioni <julian.simioni@gmail.com>
2014-12-07 16:43:51 +0100

    [scala] Improve Grade School solution
    Use an immutable Map, and use a default value to avoid getOrElse.

--- scala/grade-school/src/main/scala/grade_school.scala
+++ scala/grade-school/src/main/scala/grade_school.scala
@@ -1,15 +1,16 @@
 class School {
-  val db = scala.collection.mutable.Map[Int, Seq[String]]()
+  var db = Map[Int, Seq[String]]().withDefaultValue(Nil)
+
   def add(name : String, grade : Int) = {
-    db(grade) = db.getOrElse(grade,Seq()) ++ Seq(name)
+    db = db + (grade -> (db(grade) :+ name))
   }
 
   def grade(grade : Int) : Seq[String] = {
-    db.getOrElse(grade, Seq())
+    db(grade)
   }
 
   def sorted() : Map[Int, Seq[String]] = {
-    db.map{ case (i : Int, l : Seq[String]) =>
-      (i, l.sorted)}.toSeq.sortBy(_._1).toMap
+    db.map {
+      case (i : Int, l : Seq[String]) => (i, l.sorted)}.toSeq.sortBy(_._1).toMap
   }
 }
